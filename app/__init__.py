5fca51a8984d361ca519bd5f6982377dc2e02412
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-17 10:21:53 +0300

    ft: register main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,10 @@ def create_app(config_name):
     from config import config_options
     app.config.from_object(config_options[config_name])
 
+    # Register Blueprints
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
 
     return app
