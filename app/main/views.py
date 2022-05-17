c3e39b69783242c600c747a89c3e164af3af897e
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-17 10:40:49 +0300

    ft: add new_project view

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,15 @@
 from flask import render_template
 from . import main
+from . forms import ProjectForm
 
 @main.route('/')
 def index():
 
     return render_template('index.html')
+
+@main.route('/project/new')
+def new_project():
+
+    form = ProjectForm()
+
+    return render_template('new_project.html', form = form)
