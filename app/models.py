c47f333c560226e775060742cac121fdbacd3a33
charity-bit <charitynyanchera@gmail.com>
2022-05-18 12:44:40 +0300

    ft:login -created login and signup page

--- app/models.py
+++ app/models.py
@@ -1,11 +1,42 @@
-
+from . import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from sqlalchemy.sql import func
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
-# @login_manager.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
-    
-#     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    fullname = db.Column(db.String(255),nullable = False,unique = True)
+    role = db.Column(db.String(255),nullable = False)
+    bio = db.Column(db.String(255))
+    pic_path = db.Column(db.String(255),default='avtar.png')
+    email = db.Column(db.String(255),nullable = False,unique = True)
+    secure_password = db.Column(db.String(255),nullable = False) 
+   
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot Read Attribute Error')
+
+    @password.setter
+    def password(self,password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.secure_password,password)
+
+    def __repr__(self):
+        return f'User: {self.fullname} {self.email}'
+
+
--- manage.py
+++ manage.py
@@ -1,11 +1,37 @@
-from flask_script import Server, Manager
-from app import create_app
+from app import create_app,db
+from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
+from app.models import User
 
-app = create_app('development')
+
+app = create_app('production')
+
+
+migrate= Migrate(app,db)
+
 
 manager = Manager(app)
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
 
-manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,User = User,db = db)
+
+
+
+@manager.command
+def test():
+    """
+    Run unit tests.
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+
 
 if __name__ == '__main__':
     manager.run()
